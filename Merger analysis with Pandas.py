3c6d25bf18dacb60a5c1ad1b590b630fcfd33789
John Clements <jsclements642@gmail.com>
2016-08-28 17:43:55 -0400

    played around with pandas
    I added dataframes containing ever single pieace of 2016 data to see how
    much pandas can work with. Pandas can work with all of it.

--- Merger analysis with Pandas.py
+++ Merger analysis with Pandas.py
@@ -4,22 +4,21 @@ import sqlite3
 import os
 from sqlalchemy import create_engine # database connection
 
-if os.path.exists('E:\\Dropbox\\PS2Research\\SQL database\\'):
-    p = 'E:\\Dropbox\\PS2Research\\SQL database\\'
-else:
-    p = 'E:\\Users\\John\\Dropbox\\PS2Research\\SQL database\\'
+
+p = 'E:\\Dropbox\\PS2Research\\SQL database\\'
+
 #The first three databases connect to all nodes active in the last 44 days of nodes. The second three are the subset of nodes active in since the last check.
 Connery = sqlite3.connect(p+'Connery.db')
 Emerald = sqlite3.connect(p+'Emerald.db')
+Miller = sqlite3.connect(p+'Miller.db')
+
+CW = sqlite3.connect(p+'ConneryWeek.db')
+MW = sqlite3.connect(p+'MillerWeek.db')
+EW = sqlite3.connect(p+'EmeraldWeek.db')
 
 Emerald2016 = sqlite3.connect('E:\\Dropbox\\PS2Research\\2016Data\\Emerald.db')
 Connery2016 = sqlite3.connect('E:\\Dropbox\\PS2Research\\2016Data\\Connery.db')
 Miller2016 = sqlite3.connect('E:\\Dropbox\\PS2Research\\2016Data\\Miller.db')
-
-Miller = sqlite3.connect(p+'Miller.db')
-CW = sqlite3.connect(p+'ConneryWeek.db')
-MW = sqlite3.connect(p+'MillerWeek.db')
-EW = sqlite3.connect(p+'EmeraldWeek.db')
 
 C2 = create_engine('sqlite:///'+p+'Connery.db')
 
@@ -33,9 +32,12 @@ df_tables = df_tables[df_tables['name'].str.contains('Node') == True]
 dates = [i.replace('Node','') for i in df_tables['name']]
 
 many_dfs = []
-for x in [i for i in df_tables['name']]:
-    print(x)
-    many_dfs.append(pd.read_sql_query('SELECT * FROM %s' % x,E2016))
+for nodeTable in [i for i in df_tables['name']]:
+    edgeTable = nodeTable.replace('Node','Eset')
+    many_dfs.append([pd.read_sql_query('SELECT * FROM %s' % nodeTable,E2016),pd.read_sql_query('SELECT * FROM %s' % edgeTable,E2016)])
+    print('%s: %s, %s' % (nodeTable,len(many_dfs[-1][0]),len(many_dfs[-1][1])))
+
+
 
 
 
