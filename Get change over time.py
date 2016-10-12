4af7774cc64028ff44f10d89bc5ece7ecf18ef42
John Clements <jsclements642@gmail.com>
2016-10-12 13:09:47 -0400

    Added tracking for edge changes
    This took a few revisions but is now probably complete enough

--- Get change over time.py
+++ Get change over time.py
@@ -10,6 +10,13 @@ import time
 EmeraldEngine = create_engine('sqlite:///E:\\Dropbox\\PS2Research\\SQL database\\Emerald.db')
 
 ## Every table in the Emerald database (I think)
+
+'watersonJune15'
+
+'watersonMay18'
+
+'matthersonJune23'
+
 real = ['June30',
         'July14', 'July22', 'July29',
         'Aug4','Aug11', 'Aug18', 'Aug25',
@@ -22,9 +29,6 @@ real = ['June30',
         'Mar1',
         'Mar26_16', 'Apr2_16']
 
-'watersonJune15'
-
-'watersonMay18'
 
 def build_list_of_MandW_nodes():
     start_time = time.time()
@@ -42,7 +46,22 @@ def build_list_of_MandW_nodes():
     print("--- %s seconds --- to M and W" % (time.time() - start_time))
     return mNodes,wNodes
 
+def build_list_of_MandW_edges():
+    start_time = time.time()
+    mEdges = set()
+    for name in ['matthersonJune23e','matthersonJune15e','matthersonMay18e']:
+        df = pd.read_sql_query('SELECT Source,Target FROM %s' %name,EmeraldEngine)
+        for i,j in zip(df.Source,df.Target):
+            mEdges.add(','.join(sorted([i,j])))
 
+    wEdges = set()
+    for name in ['watersonJune23e','watersonJune15e','watersonMay18e']:
+        df = pd.read_sql_query('SELECT Source,Target FROM %s' %name,EmeraldEngine)
+        for i,j in zip(df.Source,df.Target):
+            wEdges.add(','.join(sorted([i,j])))
+    
+    print("--- %s seconds --- to M and W" % (time.time() - start_time))
+    return mEdges,wEdges
 
 ## Build or load a graph from each snapshot of the merger.
 def makeGraphs():
@@ -228,8 +247,14 @@ def node_dynamics():
     L = getGraph(real[0])
 
     ## Store all nodes removed so far.
-
+    
     removal_record = set()
+    
+    ## All the old mattherson and waterson nodes.
+
+    m,w = build_list_of_MandW_nodes()
+
+    removal_record = set(m).union(set(w)) - set(L.nodes())      
 
     ## Values for spreadsheet CP = current population, R = Leavers, PC = potentially cutoff
     CP = len(list(set(L.nodes())))
@@ -255,8 +280,6 @@ def node_dynamics():
 
         ## The symetric difference returns all Ids in G but not in L and all Ids in L but not in G
         difference = L_ids.symmetric_difference(G_ids)
-
-        ## 
 
         removed = difference.intersection(L_ids)
 
@@ -325,4 +348,218 @@ def other_node_dynamics():
 
         print('%s,%s,%s,%s' %(name,new,dead,new_dead))
         
-other_node_dynamics()
+##other_node_dynamics()
+
+## Edge dynamics:
+
+## Count the number of edges added or removed.
+
+def edge_dynamics():
+    L = getGraph(real[0],lite=True);
+
+    ## Store all edges removed so far.
+
+    removal_record = set()
+
+    ## Include the prexisting edges from M and W
+
+    m,w = build_list_of_MandW_edges()
+
+    removal_record = set(m).union(set(w)) - set(L.nodes())   
+
+    ## Values for spreadsheet CP = current population, R = Removed edges, PC = potentially cutoff edges
+    CP = len(set([','.join(sorted(x)) for x in L.edges()]))
+    R = 0
+    PC = 0
+    ## more values N = Created edges, B Edges returned from inactivity'
+    N = 0
+    B = 0
+    
+    print('Date, Current edge count, Removed edges, potentially cutoff edges, Created edges, Edges returned from inactivity')
+
+    print('%s,%s,%s,%s,%s,%s' % (real[0],CP,R,PC,N,B))
+   
+    for name in real[1:]:
+     
+        G = getGraph(name,lite=True);
+
+        ## For the sake of sanity every edge is going to be stored as a string joined by a comma with the Ids in ascending order.
+
+        L_edges = set([','.join(sorted(x)) for x in L.edges()])
+
+        G_edges = set([','.join(sorted(x)) for x in G.edges()])
+
+        ## Current edge count.
+        
+        CP = len(G_edges)
+
+        ## The symetric difference returns all Ids in G but not in L and all Ids in L but not in G
+        difference = L_edges.symmetric_difference(G_edges)
+
+        ## 
+
+        removed = difference.intersection(L_edges)
+
+        added = difference.intersection(G_edges)
+
+        R = len(removed)
+
+        ## to look at PC we need to examine every removed node and check if they are
+        ## removed because they quit or removed because they have no neighbours left
+        ## If any neighbour of both endpoints of a edge in the current graph still exists they don't get set as PC
+
+        PC = 0
+
+        ## Need G_ids to test for cutoff edges!
+
+        G_ids = set(G.nodes())
+        
+        for e in removed:
+
+            ## cutoff_flag starts true but returns False if it is in fact cut off.
+
+            cutoff_flag = True
+
+            for n in e.split(','):
+            
+                neig = set(L[n].keys())
+
+                for node in neig:
+                    if node in G_ids:
+                        cutoff_flag = False
+                        break
+
+            if cutoff_flag:
+                PC+=1
+                       
+        N = len(added)
+
+        B = len(added.intersection(removal_record))
+
+        ## Update the removal record with all removed nodes.
+        removal_record = removal_record.union(removed)
+
+        ## Print the results.
+        print('%s,%s,%s,%s,%s,%s' % (name,CP,R,PC,N,B))
+
+        ## Set the new L to be G so the process can continue
+        
+        L = G
+
+
+
+## How many nodes have completely stable edge sets?
+
+## How many triads are closed when a edge is added?
+
+## How many triangles does each new node create?
+
+def other_edge_dynamics():
+    L = getGraph(real[0],lite=True);
+
+    ## Store all edges removed so far.
+
+    removal_record = set()
+
+    ## Include the prexisting edges from M and W
+
+    m,w = build_list_of_MandW_edges()
+
+    removal_record = set(m).union(set(w)) - set(L.nodes())   
+
+    ## Values for spreadsheet SN = Stable neighbourhood, CP = number of nodes in both L and G
+    SN = 0
+    CP = 0
+    ## TC = Triads closed,TO = Triads openend, NP_TRI = triangles created by a new characters return.current population
+    TC = 0
+    TO = 0
+    NP_TRI = 0
+    
+    print('Date,Nodes in both, Stable neighbourhood, Triads closed,Triads openend, triangles created by a new')
+
+    print('%s,%s,%s,%s,%s,%s' % (real[0],CP,SN,TC,TO,NP_TRI))
+   
+    for name in real[1:]:
+     
+        G = getGraph(name,lite=True);
+
+        ## For the sake of sanity every edge is going to be stored as a string joined by a comma with the Ids in ascending order.
+
+        L_edges = set([','.join(sorted(x)) for x in L.edges()])
+
+        G_edges = set([','.join(sorted(x)) for x in G.edges()])
+
+        ## The symetric difference returns all Ids in G but not in L and all Ids in L but not in G
+        difference = L_edges.symmetric_difference(G_edges)
+
+        ## 
+
+        removed = difference.intersection(L_edges)
+
+        added = difference.intersection(G_edges)
+
+        ## Triads closed is the number of mutual neighbours of the pair as they were connected by a new edge.
+
+        TC = 0
+        for e in added:
+            a,b = e.split(',')
+            neigA = set(G[a].keys())
+
+            neigB = set(G[b].keys())
+
+            TC += len(neigA.intersection(neigB))
+
+        ## Triads opened is just the opposite.
+
+        TO = 0
+        for e in removed:
+            a,b = e.split(',')
+            neigA = set(L[a].keys())
+
+            neigB = set(L[b].keys())
+
+            TO += len(neigA.intersection(neigB))
+        
+        ## Stable neighbourhood is a count of all nodes whoes friendslist did not change.
+
+        SN = 0
+
+        ## The nodes in both G and L are the nodes that can have a stable neighbourhood.
+
+        common_L_G_nodes = set(G.nodes()).intersection(set(L.nodes()))
+
+        CP = len(common_L_G_nodes)
+
+        for n in common_L_G_nodes:
+            neigInG = set(G[n].keys())
+
+            neigInL = set(L[n].keys())
+            if (len(neigInG.symmetric_difference(neigInL)) == 0):
+                SN += 1
+
+        ## NP_TRI the number of triangles created by newcomer nodes
+        NP_TRI = 0
+
+        for e in added - removal_record:
+            a,b = e.split(',')
+
+            neigA = set(G[a].keys())
+
+            neigB = set(G[b].keys())
+
+            NP_TRI += len(neigA.intersection(neigB))
+
+        ## Update the removal record with all removed nodes.
+        removal_record = removal_record.union(removed)
+      
+        ## Print the results.
+
+        print('%s,%s,%s,%s,%s,%s' % (name,CP,SN,TC,TO,NP_TRI))
+        
+        ## Set the new L to be G so the process can continue
+        
+        L = G
+
+##other_edge_dynamics()
+
+
